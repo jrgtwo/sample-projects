d579a8740480f8516f1ca1bef66a6c8d0a72cda3
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2024-07-20 18:32:41 -0700

    feat: basic cursor with mouse down/up selection

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -1,5 +1,6 @@
 import type { Routes as RoutesType } from "../Router/types"
 import { Tabs } from "../projects/tabs"
+import { TextEditor } from "../projects/text-editor"
 import { TicTacToe } from "../projects/tic-tac-toe"
 import { UserInputs } from "../projects/user-inputs"
 import { Wordle } from "../projects/wordle"
@@ -8,5 +9,6 @@ export const Routes: RoutesType = [
   ['Tabs', '#tabs', <Tabs />],
   ['Tic Tac Toe', '#tic-tac-toe', <TicTacToe />],
   ['Wordle', '#wordle', <Wordle />],
-  ['User Inputs', '#user-inputs', <UserInputs />]
+  ['User Inputs', '#user-inputs', <UserInputs />],
+  ['Text Editor', '#text-editor', <TextEditor />]
 ]
--- src/projects/text-editor/index.tsx
+++ src/projects/text-editor/index.tsx
@@ -1,11 +1,63 @@
-
+import { useState, useEffect } from 'react'
 import './styles.css'
 
+const demoText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi rerum dolor necessitatibus numquam aliquam, inventore repellat rem aliquid commodi sunt obcaecati, quod porro esse asperiores? Mollitia obcaecati blanditiis accusantium ipsum?'
+
+
 const TextEditor = () => {
+
+  const [pages, setPages] = useState([demoText])
+  const [cursorStart, setCursorStart] = useState([0, 0])
+  const [cursorEnd, setCursorEnd] = useState([0, 0])
+
+  function handleMouseDown(event) {
+    event.preventDefault()
+
+    const curPos = event.target.dataset.idx.split(',')
+    const newCursorStart = [parseInt(curPos[0]), parseInt(curPos[1])]
+    setCursorStart(newCursorStart)
+    setCursorEnd(newCursorStart)
+  }
+  function handleMouseUp(event) {
+    event.preventDefault()
+
+    const curPos = event.target.dataset.idx.split(',')
+    const newCursorEnd = [parseInt(curPos[0]), parseInt(curPos[1])]
+    setCursorEnd(newCursorEnd)
+  }
+
   return (
-    <div>
-      <h2>A text editor will live here!</h2>
-    </div>
+    <div className="text-editor">
+      <section className="text-options">
+        <button name="bold">B</button>
+        <button name="bold">I</button>
+      </section>
+      <section className="document" onMouseUp={handleMouseUp} onMouseDown={handleMouseDown}>
+        {cursorStart} {cursorEnd}
+        {
+          pages.map((page: string, pageIdx) => (
+            <div className="page" data-id={pageIdx}>
+              <p>{
+                page.split('').map((item, index) => (
+                  <span
+                    data-idx={[pageIdx, index]}
+                    className={
+                      (
+                        (pageIdx >= cursorStart[0] && pageIdx <= cursorEnd[0])
+                        && (index >= cursorStart[1] && index <= cursorEnd[1])
+                      ) ? 'selected' : ''
+                    }
+                  >
+                    {item}
+                  </span>)
+                )
+              }</p>
+            </div>
+          ))
+        }
+
+      </section >
+    </div >
   )
 }
 
